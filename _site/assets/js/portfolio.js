d639a0cd5d8b54a51b67d75c9d297404cf5dcc0b
adithyarao3103 <adithyarao3132001@gmail.com>
2022-11-30 12:22:13 +0530

    remove site

--- _site/assets/js/portfolio.js
+++
@@ -1,9 +0,0 @@
-var headings = document.getElementsByTagName('h1');
-
-list = ''
-
-for (var i = 0; i<headings.length; i++) {
-	list +='<button class="sidebar-but" onclick=document.getElementById("' + headings[i].getAttribute('id')+'").scrollIntoView();>' + headings[i].innerHTML +'</button>';
-}
-
-document.getElementById('side-cont').innerHTML = list;
