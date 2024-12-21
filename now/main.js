07964076780c7ea552d5968db072ac492128932a
adithyarao3103 <adithyarao3132001@gmail.com>
2024-12-21 17:02:08 +0530

    now

---
+++ now/main.js
@@ -0,0 +1,33 @@
+
+function checkMobile(){
+return(parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--mobile')));
+}
+
+var menuClose=1;
+
+
+function toggleMenu(){
+if (checkMobile()){
+    if (menuClose){
+        menuClose = 0;
+        document.getElementById("navbar").classList.remove('navClose');
+        document.getElementById("navbar").classList.add('navOpen');
+    }
+    else{
+        menuClose = 1;
+        document.getElementById("navbar").classList.remove('navOpen');
+        document.getElementById("navbar").classList.add('navClose');
+    }
+}
+}
+
+
+
+menu = document.getElementById("navbar");
+menuButton = document.getElementById("navbar-mob");
+
+document.addEventListener('click', (event) => {
+if (menuClose == 0 && !menu.contains(event.target) && !menuButton.contains(event.target)) {
+toggleMenu()
+}
+});
