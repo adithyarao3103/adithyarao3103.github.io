65e8cc8a8d2b510897aa38de8e6bcbb0eb99b346
adithyarao3103 <adithyarao3132001@gmail.com>
2024-12-22 10:56:35 +0530

    update nav

--- js/main.js
+++ js/main.js
@@ -113,36 +113,40 @@ var menuClose=1;
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    if(checkMobile()){}
-    else{
-        if(scroll>100){
-            document.getElementById("navbar").classList.remove('navClose');
-            document.getElementById("navbar").classList.add('navOpen');
-            menuClose = 0;
-        }
-        else{
-            menuClose = 1;
-            document.getElementById("navbar").classList.add('navClose');
-            document.getElementById("navbar").classList.remove('navOpen');
-        }
-    }
+    navBarChange(scroll)
 });
 
-// first time
-
-if(checkMobile()){}
-else{
-    if(window.scrollY>100){
-        document.getElementById("navbar").classList.remove('navClose');
-        document.getElementById("navbar").classList.add('navOpen');
-        menuClose = 0;
+function navBarChange(scroll){
+    nav = document.getElementById("navbar");
+    if (checkMobile()){
+        nav.style.background = '#ffffff';
+        for (child of nav.children){
+            child.style.color = '#000000';
+        }
     }
     else{
-        menuClose = 1;
-        document.getElementById("navbar").classList.add('navClose');
-        document.getElementById("navbar").classList.remove('navOpen');
+        if(scroll>125){
+            nav.style.background = '#ffffff';
+            for (child of nav.children){
+                child.style.color = '#000000';
+            }
+        }
+        else{
+            nav.style.background = '#ffffff50';
+            for (child of nav.children){
+                // child.style.color = '#ffffff';
+                child.style.borderColor = 'transparent';  
+            }
+        }
     }
 }
+
+navBarChange(window.scrollY);
+
+window.onresize = function(){
+    navBarChange(window.scrollY);
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Navigation Bar - Mobile
