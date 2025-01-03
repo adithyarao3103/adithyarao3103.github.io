f21a80a6be12edf7fb18e3b5c5e924102d06be3c
adithyarao3103 <adithyarao3132001@gmail.com>
2025-01-03 13:01:15 +0530

    modify map

--- travel/main.js
+++ travel/main.js
@@ -11,7 +11,7 @@ if(checkMobile()){
 
 function style(feature) {
     return {
-        fillColor: '#f00',
+        fillColor: '#00bfff99',
         weight: 0,
         opacity: 0.1,
         color: 'white',
@@ -20,8 +20,15 @@ function style(feature) {
     };
 }
 
+const customIcon = L.icon({
+    iconUrl: 'map-marker.svg',
+    iconSize: [34, 34],     // size of the icon
+    iconAnchor: [17, 17],   // point of the icon which will correspond to marker's location
+    popupAnchor: [0, 0]   // point from which the popup should open relative to the iconAnchor
+});
+
 var map = L.map('map').setView([40.1431, 47.5769], zoom);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 maxZoom: 19,
 }).addTo(map);
 
@@ -31,6 +38,6 @@ L.geoJson(countriesData, {style:style}).addTo(map);
 
 for(var i = 0; i < cities.length; i++) {
     var city = cities[i];
-    marker = L.marker([city.Lat, city.Long], {opacity:0.9, riseOnHover:true}).addTo(map)
+    marker = L.marker([city.Lat, city.Long], {icon: customIcon, opacity:0.9, riseOnHover:true}).addTo(map)
     .bindPopup('<b>' + city.Name + '</b><br>' + ( city.Year? city.Year + '<br>' : '' ) + city.Description);
 }
